99cc0e4f05ef4c27a96f1c038f1791253bb3d78b
alexmncn78 <118453946+alexmncn78@users.noreply.github.com>
2024-06-12 03:14:51 +0200

    Improvements: Stylized data on display

--- weather_monitor_mqtt/weather_monitor_mqtt.ino
+++ weather_monitor_mqtt/weather_monitor_mqtt.ino
@@ -4,6 +4,8 @@
 #include <Fonts/FreeSans9pt7b.h>
 #include <Fonts/FreeSans12pt7b.h>
 #include <Fonts/FreeSans18pt7b.h>
+#include <Fonts/FreeSansBold24pt7b.h>
+#include <Fonts/FreeSansBold12pt7b.h> 
 
 #include <WiFi.h>
 #include <ESP32Time.h>
@@ -164,53 +166,85 @@ void showDataOnDisplay() {
   float temperature, humidity;
   getTempAndHumd(temperature, humidity);
 
+  // Get the formatted date and time
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+  String formattedDateTime = getFormattedDateTime();
+  // Extract hours and minutes
+  String hourMinute = formattedDateTime.substring(11, 16);
+
   // DISPLAY
+
+  // Background
   canvas.fillScreen(ST77XX_BLACK);
-  canvas.setCursor(0, 25);
+
+  // Temperature
   canvas.setTextColor(ST77XX_WHITE);
-  canvas.print("Temperatura: ");
+  canvas.setFont(&FreeSansBold24pt7b);
+  canvas.setCursor(35, 50);
   canvas.print(temperature);
-  canvas.println(" ºC");
+  canvas.print(" ºC");
+
+  // Humidity
+  uint16_t blue = display.color565(54, 187, 233);
+  canvas.setTextColor(blue);
+  canvas.setFont(&FreeSans12pt7b);
+  canvas.setCursor(35, 83);
   canvas.print("Humedad: ");
-  canvas.print(humidity);
+  canvas.setFont(&FreeSansBold12pt7b);
+  canvas.print((int)humidity);
   canvas.println(" %");
-  canvas.print("Bateria: ");
-  canvas.print(bat.cellPercent(), 0);
-  canvas.println("%");
 
+  // Show hour and minutes in the bottom left corner
+  canvas.setTextColor(ST77XX_WHITE);
+  canvas.setFont(&FreeSansBold12pt7b);
+  canvas.setCursor(10, 135 - 12);
+  canvas.print(hourMinute);
 
+  // Draw a battery indicator
+  int batteryLevel = bat.cellPercent();
+  int batteryHeight = map(batteryLevel, 0, 100, 0, 25);
+  int batteryWidth = map(80, 0, 100, 0, 30 * 0.8);
+  int batteryX = canvas.width() - batteryWidth - 10;
+  int batteryY = canvas.height() - 25 - 10;
+  
+  int stackWidth = 11;
+  int stackHeight = 6;
+  int stackX = batteryX + (batteryWidth - stackWidth) / 2;
+  int stackY = batteryY - stackHeight;
+  canvas.fillRect(stackX, stackY, stackWidth, stackHeight, ST77XX_WHITE); // Button
 
-  // Show all canva configuration set before
+  canvas.fillRect(batteryX, batteryY - 3, batteryWidth, 28, ST77XX_WHITE); // Battery outline
+  canvas.fillRect(batteryX + 1, batteryY + 24 - batteryHeight, batteryWidth - 2, batteryHeight, ST77XX_GREEN); // Level
+
+  // Show battery percentage
+  canvas.setTextColor(ST77XX_WHITE);
+  canvas.setFont(&FreeSansBold12pt7b);
+  canvas.setCursor(batteryX - 65, batteryY + 20);
+  canvas.print(batteryLevel);
+  canvas.print("%");
+
+  // Show all canvas configuration set before
   display.drawRGBBitmap(0, 0, canvas.getBuffer(), 240, 135);
-  pinMode(TFT_BACKLITE, OUTPUT);
-  digitalWrite(TFT_BACKLITE, HIGH);
-
   delay(5000);
 }
 
 
 void setup() {
   Serial.begin(115200);
+  // Batery
+  bat.begin();
   delay(500);
   Serial.println(" ");
 
-  // Batery
-  bat.begin();
-
-  // SET-UP display --------------------
-  // turn on the TFT / I2C power supply
-  pinMode(TFT_I2C_POWER, OUTPUT);
-  digitalWrite(TFT_I2C_POWER, HIGH);
-
-  // Init ST7789 240x135 and parameters
-  display.init(135, 240);
-  display.setRotation(3);
-  canvas.setFont(&FreeSans12pt7b);
-  canvas.setTextColor(ST77XX_WHITE);
-  //--------------------------------------
-
+  // If wake up by external reason
   esp_sleep_wakeup_cause_t wakeup_reason = esp_sleep_get_wakeup_cause();
   if (wakeup_reason == ESP_SLEEP_WAKEUP_EXT0) {
+    // Init ST7789 240x135 and parameters
+    display.init(135, 240);
+    display.setRotation(1);
+    pinMode(TFT_BACKLITE, OUTPUT);
+    digitalWrite(TFT_BACKLITE, HIGH);
+
     showDataOnDisplay();
   }
 
